c81db4b67eae9258104491de8f721d297958c01d
nurgaliev-d <nurgalyev.dias@gmail.com>
2023-01-23 18:59:06 +0600

    file

---
+++ tsis1/operators.py
@@ -0,0 +1,19 @@
+print(10+5)
+
+x = 5
+y = 4
+
+#+	#Addition	
+print(x+y)	
+#-	#Subtraction	
+print(x-y)	
+#*	#Multiplication	
+print(x*y)	
+#/	#Division	
+print(x/y)	
+#%	#Modulus	
+print(x%y)	
+#**	#Exponentiation	
+print(x**y)	
+#//	#Floor division	
+print(x//y)
---
+++ tsis1/strings.py
@@ -0,0 +1,28 @@
+print("Hello")
+print('Hello')
+a = "Hello"
+print(a)
+b = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(b)
+c = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(c)
+d = "Hello, World!"
+print(d[1])
+for x in "banana":
+  print(x)
+  e = "Hello, World!"
+print(len(e))
+txtt = "The best things in life are free!"
+print("free" in txtt)
+tt = "The best things in life are free!"
+if "free" in tt:
+  print("Yes, 'free' is present.")
+ttxt = "The best things in life are free!"
+if "expensive" not in ttxt:
+  print("No, 'expensive' is NOT present.")
