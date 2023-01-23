911332b38f1122762eda4e4aeb9962428495a578
nurgaliev-d <nurgalyev.dias@gmail.com>
2023-01-23 18:56:45 +0600

    file

--- helloworld/myfile.py
+++
@@ -1,4 +0,0 @@
-if 5 > 2:
-    print("five is greater than  two!")
-if 7 > 2:
-    print("seven is greater than  two!")
--- helloworld/variables.py
+++
@@ -1,9 +0,0 @@
-x = 5     # x is type of integer
-y = "Johnny"      # y is type of string
-print(x,y)
-
-
-
-a = 4
-A = "Sally"   # A will not  overwrite a
-print(a,A)
